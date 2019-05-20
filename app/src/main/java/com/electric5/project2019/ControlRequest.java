e9702162916cf1ea0a6245d46f2621f38586c919
Parkgeum <37042238+Parkgeum@users.noreply.github.com>
2019-05-21 02:54:27 +0900

    모빌 제어

---
+++ app/src/main/java/com/electric5/project2019/ControlRequest.java
@@ -0,0 +1,23 @@
+package com.electric5.project2019;
+
+import android.app.Activity;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ControlRequest extends PostRequest {
+
+    public ControlRequest(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        try {
+            String ip = activity.getResources().getString(R.string.ip_address);
+            url = new URL(ip + "/test");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
