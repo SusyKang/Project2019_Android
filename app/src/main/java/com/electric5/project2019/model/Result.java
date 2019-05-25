3250dd15b4cb3244da8142292a1e6e2e0727fc69
soldanam <43105016+soldanam@users.noreply.github.com>
2019-05-25 18:51:11 +0900

    //record model

---
+++ app/src/main/java/com/electric5/project2019/model/Result.java
@@ -0,0 +1,9 @@
+package com.example.uploadmp3.model;
+
+/**
+ * Created by kh on 2016. 8. 27..
+ */
+public class Result {
+
+    public String result;
+}
---
+++ app/src/main/java/com/electric5/project2019/model/UploadResult.java
@@ -0,0 +1,11 @@
+package com.example.uploadmp3.model;
+
+/**
+ * Created by kh on 2016. 8. 27..
+ */
+public class UploadResult {
+
+    public String result;
+    public String url;
+
+}
