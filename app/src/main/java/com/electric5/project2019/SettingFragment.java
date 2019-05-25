624cfb5104a5ec3cca86120ccb038893eace7343
soldanam <43105016+soldanam@users.noreply.github.com>
2019-05-25 18:56:08 +0900

    record upload완성

--- app/src/main/java/com/electric5/project2019/SettingFragment.java
+++ app/src/main/java/com/electric5/project2019/SettingFragment.java
@@ -1,12 +1,11 @@
 package com.electric5.project2019;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -16,17 +15,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-import android.widget.VideoView;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.File;
+import java.io.IOException;
+import com.electric5.project2019.model.UploadResult;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class SettingFragment extends Fragment {
+    //upload
+    ProgressDialog asyncDialog;
+    private Button uploadsound;
+    String getServerURL = "";
+    String getmp3URL="/sdcard/Music/record.wav";//폰 sdcard Music 에 녹음 파일 저장 후 upload하는 파일
 
     private MediaRecorder mMediaRecorder;
     private MediaPlayer mMediaPlayer;
-
-    final private static String RECORDED_FILE = "/recorded.mp3";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,9 +47,11 @@ public class SettingFragment extends Fragment {
 
         final Button record = (Button)view.findViewById(R.id.record);
         final Button play = (Button)view.findViewById(R.id.recordplay);
+        uploadsound= (Button)view.findViewById(R.id.recordupload);
 
         checkDangerousPermissions(); // 접근 권한 체크
 
+        getServerURL = "http://223.194.134.12:80";//서버 ip
         //녹음 버튼
         record.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -53,7 +68,7 @@ public class SettingFragment extends Fragment {
         //재생 버튼
         play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//
                 try {
                     playAudio();
                 } catch (Exception e) {
@@ -62,7 +77,116 @@ public class SettingFragment extends Fragment {
             }
         });
 
+        uploadsound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                asyncDialog = new ProgressDialog(getContext());
+                asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                asyncDialog.setMessage("로딩중입니다..");
+
+                // show dialog
+                asyncDialog.show();
+
+                uploadFile(getmp3URL);//파일 업로드
+
+            }
+        });
+
         return view;
+    }
+    //파일 업로드
+    private void uploadFile(String soundurl) {//
+
+        /**
+         * 현재 연결된 서버의 URL을 받아옴
+         */
+        String url = getServerURL;
+
+        /**
+         * 다시 연결 시도
+         */
+        // create upload service client
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .build();
+
+        NetworkService service = retrofit.create(NetworkService.class);
+
+
+        /**
+         * 서버로 보낼 파일의 전체 url을 이용해 작업
+         */
+
+        File sound = new File(soundurl);
+        RequestBody soundBody = RequestBody.create(MediaType.parse("audio/wav"), sound);
+        //audio/mpeg => mp3로 고쳐서 해보기
+        // MultipartBody.Part is used to send also the actual file name
+        MultipartBody.Part body = MultipartBody.Part.createFormData("sound", sound.getName(), soundBody);
+        Log.i("myTag","this file'name is "+ soundBody.toString());
+        Log.i("myTag","this file'name is "+ sound.getPath());
+
+
+        /**
+         * 사진 업로드하는 부분 // POST방식 이용
+         */// add another part within the multipart request
+        String descriptionString = "android";
+
+        RequestBody description = RequestBody.create(MediaType.parse("multipart/form-data"), descriptionString);
+
+
+        Call<ResponseBody> call = service.upload(body,description);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                if(response.isSuccessful()){
+
+                    Gson gson = new Gson();
+                    try {
+                        String getResult = response.body().string();
+
+                        JsonParser parser = new JsonParser();
+                        JsonElement rootObejct = parser.parse(getResult);
+
+//                        Log.i("mytag",rootObejct.toString());
+
+                        UploadResult example = gson.fromJson(rootObejct, UploadResult.class);
+
+                        Log.i("mytag",example.url);
+
+                        String result = example.result;
+
+                        if(result.equals("success")){
+                            Toast.makeText(getContext(),"업로드 성공!!!!",Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.i("MyTag", "error : "+e.getMessage());
+                    }
+
+
+                }else{
+                    Toast.makeText(getContext(),"업로드 실패!!!!",Toast.LENGTH_SHORT).show();
+                }
+
+
+                // dismiss dialog
+                asyncDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("Upload error:", t.getMessage());
+
+                // dismiss dialog
+                asyncDialog.dismiss();
+            }
+
+
+
+        });
     }
 
     final int  REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA = 1;
@@ -108,7 +232,7 @@ public class SettingFragment extends Fragment {
         killMediaPlayer();
 
         mMediaPlayer = new MediaPlayer();
-        mMediaPlayer.setDataSource(Environment.getExternalStorageDirectory().getPath()+ RECORDED_FILE);
+        mMediaPlayer.setDataSource(getmp3URL);
         mMediaPlayer.prepare();
         mMediaPlayer.start();
     }
@@ -134,7 +258,7 @@ public class SettingFragment extends Fragment {
         mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
 
-        mMediaRecorder.setOutputFile(Environment.getExternalStorageDirectory().getPath()+ RECORDED_FILE); // 파일 저장 경로
+        mMediaRecorder.setOutputFile(getmp3URL);
 
         try {
             mMediaRecorder.prepare();
@@ -153,4 +277,7 @@ public class SettingFragment extends Fragment {
 
         Toast.makeText(getActivity(), "녹음을 중단합니다", Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
