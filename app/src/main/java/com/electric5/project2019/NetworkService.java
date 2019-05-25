14c5b889c866ffaea34a9bdacad22c16bbb8ece6
soldanam <43105016+soldanam@users.noreply.github.com>
2019-05-25 18:52:44 +0900

    networkservice_record_post

---
+++ app/src/main/java/com/electric5/project2019/NetworkService.java
@@ -0,0 +1,22 @@
+package com.electric5.project2019;
+
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by kh on 2016. 8. 25..
+ */
+public interface NetworkService {
+
+    @Multipart
+    @POST("/upload")
+    Call<ResponseBody> upload(@Part MultipartBody.Part file, @Part("name") RequestBody description);
+   //Call<ResponseBody> upload(@Part ("file") RequestBody file, @Part("name") RequestBody description);
+}
